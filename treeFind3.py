dae372743304b43b7ada7f204fead66b8b6fe5d8
Aditya Verma <adityasinghverma314@gmail.com>
2019-09-20 15:25:32 +0530

    Update treeFind3.py

--- treeFind3.py
+++ treeFind3.py
@@ -1,6 +1,18 @@
 import os
 import humanize
 
+def get_size2(start_path):
+    total_size = os.path.getsize(folder)
+    for item in os.listdir(folder):
+        itempath = os.path.join(folder, item)
+        if os.path.isfile(itempath):
+            total_size += os.path.getsize(itempath)
+        elif os.path.isdir(itempath):
+            total_size += getFolderSize(itempath)
+    return (total_size)
+
+print "Size: " + str(getFolderSize("."))
+
 
 def get_size(start_path):
     total_size = 0
